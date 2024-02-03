577f46e9fb205be48d61e311f9bfafc63327eda8
amitkry <amityadavskya@gmail.com>
2024-02-03 20:37:49 +0530

    Initial commit

---
+++ obj.js
@@ -0,0 +1,12 @@
+//How to use Objects in JavaScript
+
+console.log("JS Program to store name, phone_number and marks in a object: ");
+const dict={
+    name:"Amit Kumar Yadav",
+    phone_number:7091457852,
+    marks:70
+}
+
+console.log(dict.name)
+console.log(dict.phone_number)
+console.log(dict.marks)
